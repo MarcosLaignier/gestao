e1d90eb2062d11d87d23a28d37adc78bcf68c145
Marcos Leao <marcos.leao@sonner.com.br>
2025-10-26 20:02:15 -0300

    CHANGELOG: Inciada componente de pessoa, adicionado grid list

---
+++ gestao-angular/src/app/shared/components-commons/components.commons.module.ts
@@ -0,0 +1,19 @@
+import {NgModule} from "@angular/core";
+import {TableLineComponent} from "./table-line/table.line.component";
+import {AngularMaterialModule} from "../../angular.material.module";
+import {CommonModule} from "@angular/common";
+import {TabelaComponent} from "./tabela-component/tabela.component";
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    AngularMaterialModule,
+
+  ],
+  exports:[
+  ],
+  providers: [
+  ],
+})
+export class ComponentsCommonsModule { }
