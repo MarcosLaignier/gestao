6f28bb23286e1cb8c770db88078882f784cdaef6
Marcos Leao <marcos.leao@sonner.com.br>
2025-11-08 21:14:58 -0300

    CHANGELOG:  Iniciando implementacao de componente de edicao de pessoa

--- gestao-angular/src/app/shared/components-commons/select-enum-component/select.enum.component.ts
+++ gestao-angular/src/app/shared/components-commons/select-enum-component/select.enum.component.ts
@@ -1,18 +1,62 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {FormFieldBase} from "../../utils/form.field.base";
+import {AtivoInativoEnum} from "../../enum/ativo.inativo.enum";
 
 @Component({
   selector: 'select-enum',
   templateUrl: './select.enum.component.html'
 })
-export class SelectEnumComponent  extends FormFieldBase<any>{
+export class SelectEnumComponent  {
 
   @Input() enumObject: any;
   @Input() placeholder: string = 'Selecione';
+  @Output() dataFieldChange = new EventEmitter<any>();
 
-  get enumOptions() {
-    if (!this.enumObject) return [];
-    return Object.values(this.enumObject);
+  /** Responsavel por aplicar o css do bootstrap na div externa ao component
+   *
+   */
+  @Input() colClass: string = 'col-md-2';
+
+  /** Responsavel por aplicar o label no component
+   *
+   */
+  @Input() label: string = ''
+
+
+  private _dataField!: any;
+
+  /** dataField dos componentes, com two-way dataBind
+   *
+   * @private
+   */
+  @Input()
+  get dataField(): any {
+    return this._dataField;
   }
+
+  set dataField(val: any) {
+      if (val && typeof val === 'string' && this.enumObject) {
+        const enumKeys = Object.keys(this.enumObject);
+
+        // se for chave válida do enum, mantém
+        if (enumKeys.includes(val)) {
+          this._dataField = val;
+        } else {
+          // se veio valor do enum,  encontra a chave correspondente
+          const foundKey = enumKeys.find(k => this.enumObject[k] === val);
+          this._dataField = foundKey || val;
+        }
+      } else {
+        this._dataField = val;
+      }
+
+      this.dataFieldChange.emit(this._dataField);
+
+  }
+
+  get enumOptions(): { value: unknown; key: string }[] {
+    return Object.entries(this.enumObject).map(([key, value]) => ({ key, value }));
+  }
+
 
 }
