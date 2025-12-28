f2485e043dc5f2759d8a55912fb13dd6924b9154
Marcos Laignier <marcosplaignier@hotmail.com>
2025-12-27 21:31:00 -0300

    CHANGELOG: V1 de switch component

---
+++ gestao-angular/src/app/shared/components-commons/switch-component/switch.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormFieldBase } from '../../utils/form.field.base';
+
+@Component({
+  selector: 'switch-component',
+  standalone: true,
+  templateUrl: './switch.component.html'
+})
+export class SwitchComponent extends FormFieldBase<boolean> {
+
+  id = `switch-${crypto.randomUUID()}`;
+
+  onToggle(event: Event): void {
+    const checked = (event.target as HTMLInputElement).checked;
+    this.dataField = checked;
+    this.dataFieldChange.emit(checked);
+  }
+}
