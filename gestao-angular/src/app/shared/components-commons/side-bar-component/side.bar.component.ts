4a53527a1cddab52d298b2fab57e2d6abcec41cb
Marcos Laignier <marcosplaignier@hotmail.com>
2025-12-29 16:37:06 -0300

    CHANGELOG: Melhorias na side bar

--- gestao-angular/src/app/shared/components-commons/side-bar-component/side.bar.component.ts
+++ gestao-angular/src/app/shared/components-commons/side-bar-component/side.bar.component.ts
@@ -1,5 +1,6 @@
 import {Component} from "@angular/core";
 import {RouterModule} from "@angular/router";
+import {NgClass} from "@angular/common";
 
 
 interface MenuItem {
@@ -14,48 +15,49 @@ interface MenuItem {
   selector: 'side-bar-component',
   standalone: true,
   imports: [
-    RouterModule
-],
+    RouterModule],
   templateUrl: './side.bar.component.html',
   styleUrls: ['./side.bar.component.scss']
 })
 export class SideBarComponent{
 
+  sidebarOpen = true;
+
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
+
+  toggleSubmenu(item: any) {
+    item.expanded = !item.expanded;
+  }
+
   menuItems: MenuItem[] = [
-    // {
-    //   label: 'Dashboard',
-    //   icon: 'bi-house',
-    //   route: '/dashboard'
-    // },
+    {
+      label: 'Dashboard',
+      icon: 'bi-house',
+      route: '/dashboard'
+    },
     {
       label: 'Cadastro',
       icon: 'bi-folder2-open',
       children: [
         { label: 'Pessoas', icon: 'bi-person', route: '/cadastros/pessoa' },
         // { label: 'Empresas', icon: 'bi-building', route: '/cadastro/empresas' },
-        // { label: 'Produtos', icon: 'bi-box-seam', route: '/cadastro/produtos' }
+        { label: 'Produtos', icon: 'bi-box-seam', route: '/cadastro/produtos' }
       ]
     },
-    // {
-    //   label: 'Financeiro',
-    //   icon: 'bi-cash-coin',
-    //   children: [
-    //     { label: 'Contas a Pagar', icon: 'bi-arrow-down-circle', route: '/financeiro/pagar' },
-    //     { label: 'Contas a Receber', icon: 'bi-arrow-up-circle', route: '/financeiro/receber' }
-    //   ]
-    // },
+    {
+      label: 'Financeiro',
+      icon: 'bi-cash-coin',
+      children: [
+        { label: 'Contas a Pagar', icon: 'bi-arrow-down-circle', route: '/financeiro/pagar' },
+        { label: 'Contas a Receber', icon: 'bi-arrow-up-circle', route: '/financeiro/receber' }
+      ]
+    },
     {
       label: 'Configurações',
       icon: 'bi-gear',
       route: '/configuracoes'
     }
   ];
-
-  // Fecha os outros e abre o clicado
-  toggleSubmenu(clickedItem: MenuItem): void {
-    this.menuItems.forEach(item => {
-      if (item !== clickedItem) item.expanded = false; // fecha os outros
-    });
-    clickedItem.expanded = !clickedItem.expanded; // alterna o clicado
-  }
 }
