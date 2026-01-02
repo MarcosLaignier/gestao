73fb75eede379c70964ccb830a73ef3aeb64e9ac
Marcos Laignier <marcosplaignier@hotmail.com>
2026-01-02 17:26:14 -0300

    CHANGELOG: Implementando Classe de Produto

--- gestao-angular/src/app/shared/service/empresa.service.ts
+++ gestao-angular/src/app/shared/service/empresa.service.ts
@@ -1,6 +1,5 @@
 import {HttpClient, HttpResponse} from "@angular/common/http";
 import {CrudServicePadrao} from "../utils/service/crud.service.padrao";
-import {Pessoa} from "../model/pessoa";
 import {Injectable} from "@angular/core";
 import {PessoaFilterDTO} from "../dto/filterDTO/pessoa.filter.dto";
 import {Empresa} from "../model/empresa";
